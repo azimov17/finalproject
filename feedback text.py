d546e9ccb46ed18d837a5e3c8f7ea4cb088261ce
Talantbek <73512869+azimov17@users.noreply.github.com>
2020-12-23 02:49:17 +0600

    Add files via upload

---
+++ feedback text.py
@@ -0,0 +1,39 @@
+'''1.brother, I was very surprised to see this, 
+I never thought that such codes are written 
+to make this game, your game is really good'''
+
+
+'''2.you made me really happy, I liked your game,
+ but yours was a little difficult, 
+ it was easy what we played when we were little'''
+
+
+ '''3thank you, my friend, you reminded me of my childhood'''
+
+
+ '''4.I'm just shocked, I didn't even think about
+  writing so much code for this game'''
+
+
+  '''5.friend, I'm really shocked that you made this game'''
+
+
+  '''6.it reminded me of my youth, 
+  I loved playing this game, because at that time 
+  there were only games like this'''
+
+
+  '''7.my best friend, I never thought you could make games like this.'''
+
+
+  '''8.excuse me , Talar, I don't like this game, I didn't play, 
+  but the codes you wrote attracted my interest'''
+
+
+  '''9.I used to love this game when I was little, 
+  but now there are all kinds of games, so we're not interested in these games'''
+
+
+  '''10.my teacher, you have made a very good game, 
+  can you teach me , the codes were really interested, 
+  so much code for this game is not too much'''
